a4471bb47a01c2a29332be88865f81e9f907ed18
Marek Parfianowicz <marek.parfianowicz@gmail.com>
2026-07-26 09:59:47 +0200

    OC-322: a user manual for 5.1.0

---
+++ doc/manual/5.1.0/attachments/858560677/858564183.mm
@@ -0,0 +1,23 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1336394344171" ID="ID_1887720042" MODIFIED="1427202854959" TEXT="clover-bamboo">
+<node CREATED="1336394356017" HGAP="22" ID="ID_1490915093" MODIFIED="1477395229359" POSITION="right" TEXT="releasing" VSHIFT="-46">
+<node CREATED="1336394366398" ID="ID_1920946268" MODIFIED="1477395250192" TEXT="update version number in pom.xml"/>
+</node>
+<node CREATED="1336394513651" ID="ID_784453541" MODIFIED="1336394522296" POSITION="left" TEXT="major classes">
+<node CREATED="1336394523241" ID="ID_1491717485" MODIFIED="1336394529569" TEXT="CloverAntCommandDecorator"/>
+<node CREATED="1336394531542" ID="ID_1803844597" MODIFIED="1336394541347" TEXT="CloverGrailsCommandDecorator"/>
+<node CREATED="1336394572955" ID="ID_973100577" MODIFIED="1336394578508" TEXT="CloverMavenCommandDecorator"/>
+</node>
+<node CREATED="1336394624723" ID="ID_545550960" MODIFIED="1336394629511" POSITION="right" TEXT="files">
+<node CREATED="1336394630293" ID="ID_286735400" MODIFIED="1336394642878" TEXT="*.ftl - freemarker template language"/>
+<node CREATED="1336394662352" ID="ID_1459567672" MODIFIED="1477395206352" TEXT="source code stored in bamboo repository"/>
+<node CREATED="1336394679822" ID="ID_1603654756" MODIFIED="1477395210320" TEXT="components/bamboo-plugins2/bamboo-plugin-clover"/>
+</node>
+<node CREATED="1336394714151" ID="ID_1667391091" MODIFIED="1336394848289" POSITION="right" TEXT="atlassian-plugin.xml" VSHIFT="28">
+<node CREATED="1336394731933" ID="ID_456900601" MODIFIED="1336394750153" TEXT="connectors to ant / maven / grails builders"/>
+<node CREATED="1336394751961" ID="ID_1817136440" MODIFIED="1336394759014" TEXT="pre / post build actions"/>
+<node CREATED="1336394771722" ID="ID_1599738039" MODIFIED="1336394830275" TEXT="xwork / web-item - define presentation of clover results within bamboo"/>
+</node>
+</node>
+</map>
---
+++ doc/manual/5.1.0/attachments/858560677/858564192.mm
@@ -0,0 +1,17 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1336395248156" ID="ID_791470654" MODIFIED="1336395255958" TEXT="clover-maven">
+<node CREATED="1336395269251" ID="ID_803386630" MODIFIED="1336395292840" POSITION="right" TEXT="supports  java and groovy"/>
+<node CREATED="1336395293770" ID="ID_392582047" MODIFIED="1336395310272" POSITION="left" TEXT="project files">
+<node CREATED="1336395311088" ID="ID_1786193728" MODIFIED="1336395313997" TEXT="src/it">
+<node CREATED="1336395315436" ID="ID_11773944" MODIFIED="1336395322474" TEXT="integration tests (full maven projects)"/>
+</node>
+<node CREATED="1336395326969" ID="ID_1625901730" MODIFIED="1477394544632" TEXT="DistributedCoverage class">
+<node CREATED="1336395336336" ID="ID_1418733275" MODIFIED="1336395353054" TEXT="created in the same package as Mojo&apos;s (maven limitation)"/>
+</node>
+<node CREATED="1336395357803" ID="ID_519355964" MODIFIED="1336395363218" TEXT="AbstractInstrumenter">
+<node CREATED="1336395364026" ID="ID_1906587184" MODIFIED="1336395372675" TEXT="calls CloverInstrumenter"/>
+</node>
+</node>
+</node>
+</map>
